53c07561ee7b4fa0b827de30f98cd79da1f60abe
Farhanayesmin2 <farhanayesmin@gmail.com>
2023-03-03 21:09:37 +0600

    Modal added

--- js/categories.js
+++ js/categories.js
@@ -59,15 +59,14 @@ const loadCategoriesDisplay = (categories,dataLimit) => {
 
 
                     <div class="text-end ">
-                        <a class="text-pink-500 inline-flex items-center md:mb-2 lg:mb-0 justify-items-end">
-                        <button type="submit"  >   <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"
+                   
+                        <button class="text-pink-500 inline-flex items-center md:mb-2 lg:mb-0 justify-items-end">
+                        <label for="my-modal-3"><svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"
                                 fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path d="M5 12h14"></path>
                                 <path d="M12 5l7 7-7 7"></path>
-                            </svg></button>
-                         
-                        </a>
-                       
+                            </svg>  </label>
+                  </button>
      
                     </div>
                                 <i class="fa-solid fa-calendar-days"></i> <span>${category.published_in
