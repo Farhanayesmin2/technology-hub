4dea425b39390f64b9659beaeac6e17019eccac7
Farhanayesmin2 <farhanayesmin@gmail.com>
2023-03-03 14:13:03 +0600

    Image loaded

--- js/categories.js
+++ js/categories.js
@@ -35,10 +35,10 @@ const loadCategoriesDisplay = (categories) => {
     //   categoryDiv.classList.add("col");
        
             categoryDiv.innerHTML = `
-<div class=" m-4">
+<div class="shadow-lg shadow-gray-500 m-4">
         <div class="p-4  ">
             <div class="h-[100%] w-[100%] border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                <img class=" md:h-36 w-full object-cover object-center" src="https://dummyimage.com/720x400"
+                <img class="rounded-lg md:h-36 w-full object-cover object-center" src=${category.image}
                     alt="blog">
                 <div class="p-6">
                     <h2 class="tracking-widest text-lg title-font font-bold text-gray-800 mb-1">Features</h2>
@@ -54,30 +54,23 @@ const loadCategoriesDisplay = (categories) => {
                 
                    </ol>
                    </div>
-                    <div class="flex items-center flex-wrap ">
-                        <a class="text-pink-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
-                            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"
+                   <hr>
+<div>
+<h1 class="text-md  font-bold" >${category.name}</h1>
+</div>
+
+
+                    <div class="text-end ">
+                        <a class="text-pink-500 inline-flex items-center md:mb-2 lg:mb-0 justify-items-end">
+                        <button type="submit"  >   <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"
                                 fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path d="M5 12h14"></path>
                                 <path d="M12 5l7 7-7 7"></path>
-                            </svg>
+                            </svg></button>
+                         
                         </a>
-                        <span
-                            class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none"
-                                stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                                <circle cx="12" cy="12" r="3"></circle>
-                            </svg>1.2K
-                        </span>
-                        <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-                            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none"
-                                stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                                <path
-                                    d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z">
-                                </path>
-                            </svg>6
-                        </span>
+                       
+                       
                     </div>
                 </div>
             </div>
@@ -85,7 +78,7 @@ const loadCategoriesDisplay = (categories) => {
      
         postContainer.appendChild(categoryDiv);
        
-        console.log(category.features);
+        console.log(category);
     }
 
     };
